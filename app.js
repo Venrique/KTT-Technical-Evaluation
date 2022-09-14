5d06047073fa57f1feafb41f6ac841ca3ac1151a
Venrique <00046517@uca.edu.sv>
2022-09-13 23:58:45 -0600

    Vaccines administered page almost done

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.set('view engine', 'ejs');
 app.use('/', indexRouter);
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/images', express.static(__dirname + '/public/images'));
+app.use('/js', express.static(__dirname + '/public/js'));
 
 app.listen(3000, () =>{
     console.log("Servidor inicializado en el puerto 3000")
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,64 @@
-var express = require('express');
-var router = express.Router();
-
-const API_URL = "https://covid-api.mmediagroup.fr/v1/cases"
+const { json } = require('express');
+const express = require('express');
+const router = express.Router();
+const fetch = require("node-fetch");
 
 
 router.get('/', function (req, res) {
-    res.render('index',{title: 'KTT Technical Evaluation', page: 'confirmedCases'});
+    res.render('index',{title: 'KTT Technical Evaluation', page: 'home'});
 
 });
+
+router.get('/confirmed', async (req, res) => {
+    const API_URL = "https://covid-api.mmediagroup.fr/v1/cases?country=france"
+    const options = {
+        "method": "GET",
+    }
+
+    const response = await fetch(API_URL, options)
+        .then(res => res.json())
+        .catch(e => {
+            console.error({
+                "message": "oh no",
+                error: e,
+            });
+        });
+    console.log(response);
+    res.json(response);
+    //res.render('index',{title: 'KTT Technical Evaluation', page: 'confirmedCases'});
+});
+
+router.get('/vaccines', async (req, res) => {
+    const API_URL = "https://covid-api.mmediagroup.fr/v1/vaccines"
+    const options = {
+        "method": "GET",
+    }
+
+    const response = await fetch(API_URL, options)
+        .then(res => res.json())
+        .catch(e => {
+            console.error({
+                "message": "oh no",
+                error: e,
+            });
+        });
+    
+    var vac = {}
+    var key = 'Vaccines'
+    vac[key] = [];
+    
+    Object.keys(response).forEach(element => {
+        var data = {
+            country: element,
+            administered: response[element].All.administered,
+            vaccinated: response[element].All.people_vaccinated,
+            partially: response[element].All.people_partially_vaccinated
+        }
+        vac[key].push(data);
+    });
+
+    res.render('index',{title: 'KTT Technical Evaluation', page: 'confirmedCases', vac});
+});
+
 
 module.exports = router;
