8bdb341f48bf64152de22c4fb2571380b230c6f3
Venrique <00046517@uca.edu.sv>
2022-09-15 00:27:19 -0600

    Project almost finished. Death and confirmed cases added.

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ const fetch = require("node-fetch");
 
 
 router.get('/', function (req, res) {
-    res.render('index',{title: 'KTT Technical Evaluation', page: 'home'});
+    res.render('index', { title: 'KTT Technical Evaluation', page: 'home' });
 
 });
 
 router.get('/confirmed', async (req, res) => {
-    const API_URL = "https://covid-api.mmediagroup.fr/v1/cases?country=france"
+    const API_URL = "https://covid-api.mmediagroup.fr/v1/cases"
     const options = {
         "method": "GET",
     }
@@ -23,9 +23,21 @@ router.get('/confirmed', async (req, res) => {
                 error: e,
             });
         });
-    console.log(response);
-    res.json(response);
-    //res.render('index',{title: 'KTT Technical Evaluation', page: 'confirmedCases'});
+
+    var confirmed = {}
+    var key = 'Confirmed'
+    confirmed[key] = [];
+
+    Object.keys(response).forEach(element => {
+        var data = {
+            country: element,
+            population: response[element].All.population,
+            confirmed: response[element].All.confirmed
+        }
+        confirmed[key].push(data);
+    });
+
+    res.render('index',{title: 'KTT Technical Evaluation', page: 'confirmed', confirmed });
 });
 
 router.get('/vaccines', async (req, res) => {
@@ -42,11 +54,11 @@ router.get('/vaccines', async (req, res) => {
                 error: e,
             });
         });
-    
+
     var vac = {}
     var key = 'Vaccines'
     vac[key] = [];
-    
+
     Object.keys(response).forEach(element => {
         var data = {
             country: element,
@@ -57,7 +69,49 @@ router.get('/vaccines', async (req, res) => {
         vac[key].push(data);
     });
 
-    res.render('index',{title: 'KTT Technical Evaluation', page: 'confirmedCases', vac});
+    res.render('index', { title: 'KTT Technical Evaluation', page: 'vaccines', vac });
+});
+
+router.get('/deaths', async (req, res) => {
+
+    const API_URL = "https://covid-api.mmediagroup.fr/v1/history?status=deaths"
+    const options = {
+        "method": "GET",
+    }
+
+    const response = await fetch(API_URL, options)
+        .then(res => res.json())
+        .catch(e => {
+            console.error({
+                "message": "oh no",
+                error: e,
+            });
+        });
+
+    var deaths = {}
+    var key = 'Deaths'
+    deaths[key] = [];
+    
+    let currDate = new Date();
+    let date_ob = new Date(currDate.getTime() - 86400000*2)
+
+    // adjust 0 before single digit date
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    // current month
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    // current year
+    let year = date_ob.getFullYear();
+    Object.keys(response).forEach(element => {
+        var data = {
+            country: element,
+            population: response[element].All.population,
+            dead: response[element].All.dates[year + "-" + month + "-" + date],
+        }
+        deaths[key].push(data);
+    });
+
+
+    res.render('index', { title: 'KTT Technical Evaluation', page: 'deaths', deaths });
 });
 
 
